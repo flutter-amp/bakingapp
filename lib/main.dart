d3505dac7b51997ccabf92d26aaa43cb35c33b40
Adona Tesfaye <adonatesfaye99@gmail.com>
2021-02-13 18:40:50 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,25 @@
 import 'package:baking_app/Baking/bloc/recipe_bloc/recipe_bloc.dart';
 import 'package:baking_app/Baking/view/screens/baking_route.dart';
-import 'package:baking_app/tabsNavigation.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import './Baking//bloc/recipe_bloc/recipe_event.dart';
+
 import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
+
+import 'Baking/bloc/recipe_bloc/recipe_event.dart';
 
 void main() {
   runApp(
     BlocProvider(
 
-      create: (context)=>RecipeBloc(),
+      create: (context)=>RecipeBloc()..add(RecipeRetrieve()),
       child: MyApp(),
 
     ),
   );
 }
 
-class RecipeRetreive {
-}
+
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
